2ee4140143717b305a8778193b339aa76e89c503
liuhongping <hongpingliu@outlook.com>
2016-05-13 09:34:31 +0800

    teset

--- SpringJDBC/SpringJdbcApplication.java
+++ SpringJDBC/SpringJdbcApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringJdbcApplication{
 
 	public static void main(String[] args) {
-		
 		SpringApplication.run(SpringJdbcApplication.class, args);
 	}
 
